e66824a906929865c23c0883f9d51ca08cb75122
Dan Kessler <dankessler@gmail.com>
2018-05-05 16:32:03 -0400

    Tweak settings for final run

--- SBM_Inference/Run_Test.m
+++ SBM_Inference/Run_Test.m
@@ -9,9 +9,11 @@ sanity=0;
 
 %% Pre-shuffle the labels and run again as a sanity check
 path = '~/Documents/hcp_data/HCP_components.mat';
-nperms = 100;
+nperms = 10000;
 topComps = 809;
 dropNets = [-1];
 sanity=1;
 
 [fake.pval,fake.log_lik_true,fake.log_lik_null_median] = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
+
+save('SBM_Inference.mat','sbm');
