9cacf9197b706bd961519591eba2b46c271fa62c
Dan Kessler <dankessler@gmail.com>
2018-05-04 22:39:22 -0400

    update testing script to capture real and null ll

--- SBM_Inference/Run_Test.m
+++ SBM_Inference/Run_Test.m
@@ -1,15 +1,17 @@
+%% Real Run
 path = '~/Documents/hcp_data/HCP_components.mat';
 nperms = 10000;
 topComps = 809;
 dropNets = [-1];
 sanity=0;
 
-ps_real = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
+[sbm.pval,sbm.log_lik_true,sbm.log_lik_null_median] = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
 
+%% Pre-shuffle the labels and run again as a sanity check
 path = '~/Documents/hcp_data/HCP_components.mat';
 nperms = 100;
 topComps = 809;
 dropNets = [-1];
 sanity=1;
 
-ps_fake = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
+[fake.pval,fake.log_lik_true,fake.log_lik_null_median] = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
