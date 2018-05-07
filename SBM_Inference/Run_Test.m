62e3caf18cb0ba4f5e07e990e57922b5790aed80
Daniel Kessler <kesslerd@umich.edu>
2018-05-06 20:24:29 -0400

    Further clean up run code

--- SBM_Inference/Run_Test.m
+++ SBM_Inference/Run_Test.m
@@ -1,6 +1,6 @@
+%% Real Run
 rng(2018)
 
-%% Real Run
 path = '~/Documents/hcp_data/HCP_components.mat';
 nperms = 20000;
 topComps = 809;
@@ -8,8 +8,11 @@ dropNets = [-1];
 sanity=0;
 
 [sbm.pval,sbm.log_lik_true,sbm.log_lik_null_median] = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
+save('SBM_Inference.mat','sbm');
 
 %% Pre-shuffle the labels and run again as a sanity check
+rng(2018)
+
 path = '~/Documents/hcp_data/HCP_components.mat';
 nperms = 20000;
 topComps = 809;
@@ -18,4 +21,4 @@ sanity=1;
 
 [fake.pval,fake.log_lik_true,fake.log_lik_null_median] = FundamentalUnits_SBM_Test(path,nperms,topComps,dropNets,sanity);
 
-save('SBM_Inference.mat','sbm');
+save('SBM_Fake_Inference.mat','fake');
